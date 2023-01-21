f52bbcad17f102dfd8116818343f99bc799b9f05
Justin <justin.prod@kakaoent.com>
2023-01-21 14:18:39 +0900

    Order 서비스 기본 데이터 세팅

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO order_table (id, customer_name, product_id) VALUES (1, '김재은', 1);
+INSERT INTO order_table (id, customer_name, product_id) VALUES (2, '이문희', 1);
+INSERT INTO order_table (id, customer_name, product_id) VALUES (3, '김이서', 4);
+INSERT INTO order_table (id, customer_name, product_id) VALUES (4, '이문희', 5);
+INSERT INTO order_table (id, customer_name, product_id) VALUES (5, '김재은', 2);
